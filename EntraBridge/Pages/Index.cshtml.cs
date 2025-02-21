be85874eb2ac6f67ca4b8e6719024f5b25cfdadf
Roberto Borges <roberto.borges@outlook.com>
2025-02-21 14:41:39 -0500

    Add initial project structure and configuration files

---
+++ EntraBridge/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Web;
+using System.Net;
+using Microsoft.Graph;
+
+namespace EntraBridge.Pages;
+
+[AuthorizeForScopes(ScopeKeySection = "MicrosoftGraph:Scopes")]
+public class IndexModel : PageModel
+{
+    private readonly GraphServiceClient _graphServiceClient;
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger, GraphServiceClient graphServiceClient)
+    {
+        _logger = logger;
+        _graphServiceClient = graphServiceClient;
+    }
+
+    public async Task OnGet()
+    {
+        try
+        {
+            var user = await _graphServiceClient.Me.Request().GetAsync();
+            ViewData["GraphApiResult"] = user.DisplayName; ;
+        }
+        catch (Exception ex)
+        {
+
+            throw ex;
+        }
+
+        
+
+    }
+}
